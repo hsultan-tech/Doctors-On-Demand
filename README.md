2ef140898936b2aff4f5bc2125add84ed46d33a5
Hamad Sultan <hsultan@uwaterloo.ca>
2025-01-11 02:43:42 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# winHacks
+WinHacks Project
---
+++ app/src/main/java/com/example/winhacks/Covid19InfoActivity.kt
@@ -0,0 +1,21 @@
+package com.example.winhacks
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.TextView
+import okhttp3.*
+import org.json.JSONObject
+import java.io.IOException
+
+class Covid19InfoActivity : AppCompatActivity() {
+
+    private val client = OkHttpClient()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_covid19_info)
+
+//        run("https://corona.lmao.ninja/all")
+    }
+}
